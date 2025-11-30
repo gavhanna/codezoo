c5135b6ea38ed023dc15bd53b976afc76d5c7055
Gavin Hanna <gavhanna@gmail.com>
2025-11-30 09:54:01 +0000

    Move datasource to its own file to accomodate prisma v7 change

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,13 +1,25 @@
+import { PrismaPg } from '@prisma/adapter-pg'
 import { PrismaClient } from '@prisma/client'
+import { Pool } from 'pg'
 
 declare global {
   // eslint-disable-next-line no-var
   var prismaGlobal: PrismaClient | undefined
 }
 
+const databaseUrl = process.env.DATABASE_URL
+
+if (!databaseUrl) {
+  throw new Error('DATABASE_URL is not set')
+}
+
+const pool = new Pool({ connectionString: databaseUrl })
+const adapter = new PrismaPg(pool)
+
 const prisma =
   globalThis.prismaGlobal ??
   new PrismaClient({
+    adapter,
     log: process.env.NODE_ENV === 'development' ? ['query', 'warn', 'error'] : ['error'],
   })
 
